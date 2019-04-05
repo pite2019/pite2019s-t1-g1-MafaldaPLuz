9df4a1602878b923f7dfcf37c153cb12a91dd18b
Mafalda Pinheiro Luz <48191234+MafaldaPLuz@users.noreply.github.com>
2019-04-05 22:42:43 +0200

    Update task.py

--- task.py
+++ task.py
@@ -3,55 +3,54 @@ from matrix import Matrix
 def main():
     matrix1 = Matrix(1,2,3,4)
     matrix2 = Matrix(5,6,7,8)
-    print ("The first Matrix is : " )
+    print ('The first Matrix is : ' )
     matrix1.printMatrix()
-    print ("")
+    print ('')
     
-    print ("The second Matrix is : " )
+    print ('The second Matrix is : ' )
     matrix2.printMatrix()
-    print ("")
+    print ('')
   
-    print ("m1+=m2:")
-    matrix1+=matrix2
+    print ('m1+=m2:')
+    matrix1 += matrix2
     matrix1.printMatrix()
-    print ("")
+    print ('')
     
-    print ("m3=m1+m2:")
-    matrix3=matrix1+matrix2
+    print ('m3=m1+m2:')
+    matrix3 = matrix1+matrix2
     matrix3.printMatrix()
-    print ("")
+    print ('')
     
-    print ("m1-=m2:")
-    matrix1-=matrix2
+    print ('m1-=m2:')
+    matrix1 -= matrix2
     matrix1.printMatrix()
-    print ("")
+    print ('')
     
-    print ("m4=m1-m2:")
-    matrix4=matrix1-matrix2
+    print ('m4=m1-m2:')
+    matrix4 = matrix1 - matrix2
     matrix4.printMatrix()
-    print ("")
+    print ('')
     
-    print ("m5=m1@m2:")
-    matrix5=matrix1@matrix2
+    print ('m5=m1@m2:')
+    matrix5 = matrix1 @ matrix2
     matrix5.printMatrix()
-    print ("")
+    print ('')
     
-    print ("m6=m1+2:")
-    matrix6=matrix1+2
+    print ('m6=m1+2:')
+    matrix6 = matrix1 + 2
     matrix6.printMatrix()
-    print ("")
+    print ('')
     
-    print ("m7=2+m1:")
-    matrix7=2+matrix1
+    print ('m7=2+m1:')
+    matrix7 = 2 + matrix1
     matrix7.printMatrix()
-    print ("")
+    print ('')
     
-    print ("m8=m1-2:")
-    matrix8=matrix1-2
+    print ('m8=m1-2:')
+    matrix8 = matrix1 - 2
     matrix8.printMatrix()
-    print ("")
+    print ('')
     
-   
     
-if __name__== "__main__":
+if __name__== '__main__':
     main()
